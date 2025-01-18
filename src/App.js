0c13b36eaa4fc77b435640d3e8d0a895ab759839
jabez-abija2399 <jabeznew2399@gmail.com>
2025-01-18 22:12:00 +0300

    feat: enhance App component layout with MUI Box and Typography

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { Box, Paper, Typography } from "@mui/material";
 
 function App() {
   return (
-
+    <>
+    <Box component={Paper} sx={{ maxWidth: 400, margin: '20px auto', padding: 2, backgroundColor: 'lightblue'}}>
+      <Typography variant="h4" align="center">To Do</Typography>
+    </Box>
+    </>
   );
 }
 
