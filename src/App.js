681b69a188f1d64de558cd4140449f6b244c55ad
jabez-abija2399 <jabeznew2399@gmail.com>
2025-01-18 22:54:26 +0300

    feat: update App component layout and integrate Task component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Box, Paper, Typography } from "@mui/material";
+import Task from "./tasks";
 
 function App() {
   return (
     <>
-    <Box component={Paper} sx={{ maxWidth: 400, margin: '20px auto', padding: 2, backgroundColor: 'lightblue'}}>
+    <Box component={Paper} sx={{ maxWidth: 800, margin: '100px auto', padding: 2, backgroundColor: '#f4c0e5' }}>
       <Typography variant="h4" align="center">To Do</Typography>
+      <Task />
     </Box>
     </>
   );
