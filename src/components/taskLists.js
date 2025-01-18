11c78e2f4917c2a3c7635ec5f09f6150b335b4ca
jabez-abija2399 <jabeznew2399@gmail.com>
2025-01-18 22:54:46 +0300

    feat: add ListOfTasks component to manage a list of tasks with completion status

---
+++ src/components/taskLists.js
@@ -0,0 +1,6 @@
+const ListOfTaks= [
+    { text: 'Learn React basics', completed: false },
+    { text: 'Build a To-Do List app', completed: true },
+    { text: 'Explore advanced React concepts', completed: false },
+  ]
+export default ListOfTaks;
