fe8a883324b0614796f7ee64a457cd7c73c9cf6b
jabez-abija2399 <jabeznew2399@gmail.com>
2025-01-18 23:46:05 +0300

    refactor: remove deprecated taskLists and Task components

--- src/components/taskLists.js
+++
@@ -1,6 +0,0 @@
-const ListOfTaks= [
-    { text: 'Learn React basics', completed: false },
-    { text: 'Build a To-Do List app', completed: true },
-    { text: 'Explore advanced React concepts', completed: false },
-  ]
-export default ListOfTaks;
