c2345df6555a8bcb11f4782a1251c5157e7b141d
jabez-abija2399 <jabeznew2399@gmail.com>
2025-01-20 12:53:46 +0300

    feat: enhance App component with AppBar, Container, and ScrollTop functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import { Box, Paper, Typography } from "@mui/material";
+import React from "react";
+import {PropTypes} from "prop-types";
+import { Box, CssBaseline, Fab, Typography, Paper, AppBar, Toolbar, Container } from "@mui/material";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
+import Fade from "@mui/material/Fade";
 import Task from "./components/tasks";
+import ScrollTop from "./components/scrollTop";
 
-function App() {
+
+function App(props) {
   return (
     <>
+    <CssBaseline />
+    {/* AppBar at the top */}
+
+    {/* Main To-Do List Section */}
+    <Container>
       <Box
         component={Paper}
         sx={{
@@ -12,16 +24,25 @@ function App() {
           maxWidth: 800,
           margin: "100px auto",
           padding: 2,
-          backgroundColor: "#f4c0e5",
+          backgroundColor: "#384727dc",
         }}
       >
         <Typography variant="h4" align="center">
           To Do
         </Typography>
-
         <Task />
+        
       </Box>
-    </>
+    </Container>
+
+    {/* Scroll to Top Button */}
+    {/* <ScrollTop {...props}>
+      <Fab size="small" aria-label="scroll back to top">
+        <KeyboardArrowUpIcon />
+      </Fab>
+    </ScrollTop> */}
+  </>
+
   );
 }
 
