a5bc60a1f9af8278dcfb1e5b0167147124137b29
jabez-abija2399 <jabeznew2399@gmail.com>
2025-01-20 11:39:59 +0300

    feat: implement Task component to render tasks with edit and delete actions

--- src/components/tasks.jsx
+++ src/components/tasks.jsx
@@ -1,110 +1,63 @@
-// import React, { useState } from "react";
-// import ListOfTasks from "./components/taskLists";
-// import { Box, Paper, List, ListItem } from "@mui/material";
-// import ListItemIcon from '@mui/material/ListItemIcon';
-// import Checkbox from '@mui/material/Checkbox';
-
-// const Task = () => {
-//   return (
-// <Box
-//   component={Paper}
-//   sx={{ maxWidth: 800, margin: "20px auto", padding: 2 }}
-// >
-// <List component={Paper}>
-//   {ListOfTasks.map((task, index) => (
-//     <Box
-//       key={index}
-//       sx={{
-//         backgroundColor: "#f4c0e5",
-//         padding: 1,
-//         margin: 1,
-//         borderRadius: 5,
-//       }}
-//     >
-//       <ListItem>
-//       <ListItemIcon>
-//           <Checkbox
-//             edge="start"
-//           //   checked={checked.includes(value)}
-//             tabIndex={-1}
-//             disableRipple
-//           //   inputProps={{ 'aria-labelledby': labelId }}
-//           />
-//         </ListItemIcon>
-
-//           {task.text}
-//       </ListItem>
-//     </Box>
-//   ))}
-// </List>
-//     </Box>
-//   );
-// };
-
-// export default Task;
-
-// src/components/TaskRow.js
-import React from "react";
-import {
-  Box,
-  Checkbox,
-  ListItem,
-  ListItemIcon,
-  Typography,
-  List,
-  Paper,
-} from "@mui/material";
+import React, { useState } from "react";
+import ListOfTasks from "../taskLists";
+import { Box, Paper, List, ListItem, Typography } from "@mui/material";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Checkbox from "@mui/material/Checkbox";
 import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
 import IconButton from "@mui/material/IconButton";
-import { Edit } from "@mui/icons-material";
 
-const TaskRow = ({ task }) => {
+const Task = () => {
   return (
-    // <Box style={style} sx={{ padding: 1, backgroundColor: "#fff", margin: "5px 0", borderRadius: 5 }}>
     <Box
       component={Paper}
       sx={{ maxWidth: 800, margin: "20px auto", padding: 2 }}
     >
       <List component={Paper}>
-        {/* {ListOfTasks.map((task, index) => ( */}
-        <Box
-          // key={index}
-          sx={{
-            backgroundColor: "#f4c0e5",
-            padding: 1,
-            margin: 1,
-            borderRadius: 5,
-          }}
-        >
-          <ListItem sx={{
-              display: "flex",
-              justifyContent: "space-between", // Align content to both ends
-              alignItems: "center", // Vertically center the content
-            }}>
-            <Box
+        {ListOfTasks.map((task, index) => (
+          <Box
+            key={index}
+            sx={{
+              backgroundColor: "#f4c0e5",
+              padding: 1,
+              margin: 1,
+              borderRadius: 5,
+            }}
+          >
+            <ListItem
               sx={{
                 display: "flex",
-                alignItems: "center", // Vertically center the checkbox and text
+                justifyContent: "space-between",
+                alignItems: "center",
               }}
             >
+              <Box sx={{display: "flex", alignItems: "center"}}>
               <ListItemIcon>
-                <Checkbox edge="start" tabIndex={-1} disableRipple />
+                <Checkbox
+                  edge="start"
+                  //   checked={checked.includes(value)}
+                  tabIndex={-1}
+                  disableRipple
+                  //   inputProps={{ 'aria-labelledby': labelId }}
+                />
               </ListItemIcon>
+
               <Typography>{task.text}</Typography>
-            </Box>
-            <IconButton edge="start" aria-label="delete">
-              <Edit  />
-            </IconButton>
-            <IconButton edge="start" aria-label="delete">
+              </Box>
+              <Box sx={{display: "flex", alignItems: "center"}}>
+              <IconButton edge="start" aria-label="edit">
+                <EditIcon />
+              </IconButton>
+              <IconButton edge="start" aria-label="delete">
               <DeleteIcon />
             </IconButton>
-
-          </ListItem>
-        </Box>
-        {/* ))} */}
+            </Box>
+            </ListItem>
+          </Box>
+        ))}
       </List>
     </Box>
   );
 };
 
-export default TaskRow;
+export default Task;
