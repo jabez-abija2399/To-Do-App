8e62711f78249ac2ebe73379362b805bd22de2df
jabez-abija2399 <jabeznew2399@gmail.com>
2025-01-20 13:40:27 +0300

    feat: add scroll functionality to Task component with scroll to top and bottom buttons

--- src/components/tasks.jsx
+++ src/components/tasks.jsx
@@ -1,13 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import ListOfTasks from "../taskLists";
-import { Box, Paper, List, ListItem, Typography } from "@mui/material";
+import { Box, Paper, List, ListItem, Typography, Button } from "@mui/material";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import Checkbox from "@mui/material/Checkbox";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
 import IconButton from "@mui/material/IconButton";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 
 const Task = () => {
+  const listRef = useRef(null);
+
+  // fuction to handle the scrooll to the Top of the list
+  const scrollTop = () => {
+    if (listRef.current) {
+      listRef.current.scrollTop = 0;
+    }
+  };
+
+  // function to handle the scrooll to the Bottom of the list
+  const scrollBottom = () => {
+    if (listRef.current) {
+      listRef.current.scrollTop = listRef.current.scrollHeight;
+    }
+  };
+  
   return (
     <Box
       component={Paper}
@@ -15,11 +33,19 @@ const Task = () => {
         maxWidth: 800,
         margin: "20px auto",
         padding: 2,
-        maxHeight: 450, // Set the maximum height for the list container.
-        overflowY: "auto", // Enable vertical scrolling when content exceeds max height.
+        position: "relative",
       }}
     >
-      <List component={Paper}>
+      <List
+        component={Paper}
+        ref={listRef}
+        sx={{
+          maxHeight: 400,
+          overflowY: "auto",
+          overflowX: "hidden",
+          padding: 1,
+        }}
+      >
         {ListOfTasks.map((task, index) => (
           <Box
             key={index}
@@ -55,13 +81,37 @@ const Task = () => {
                   <EditIcon />
                 </IconButton>
                 <IconButton edge="start" aria-label="delete">
-                  <DeleteIcon />
+                  <DeleteIcon sx={{color: "red"}} />
                 </IconButton>
               </Box>
             </ListItem>
           </Box>
         ))}
       </List>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          // marginTop: 2,
+          position: "absolute",
+          alignItems: "center", 
+          left: "50%",
+          
+        }}
+      >
+        <IconButton
+          sx={{ backgroundColor: "#84f504dc", borderRadius: "50%", boxShadow: 4  }}
+          onClick={scrollTop}
+        >
+          <KeyboardArrowUpIcon sx={{color: "black"}} />
+        </IconButton>
+        <IconButton
+          sx={{ backgroundColor: "#84f504dc", borderRadius: "50%", boxShadow: 2 }}
+          onClick={scrollBottom}
+        >
+          <KeyboardArrowDownIcon sx={{color: "black"}}  />
+        </IconButton>
+      </Box>
     </Box>
   );
 };
