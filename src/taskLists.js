66ff341c021ae3d730a6ed3d519143ac883a66b3
jabez-abija2399 <jabeznew2399@gmail.com>
2025-01-20 17:53:42 +0300

    feat: rename 'text' property to 'title' in task list for improved clarity

--- src/taskLists.js
+++ src/taskLists.js
@@ -1,12 +1,12 @@
 const ListOfTasks = [
-  { text: "Learn React basicsLearn React basicsLearn React basicsLearn React basicsLearn React basics", completed: false },
-  { text: "Build a To-Do List app", completed: true },
-  { text: "Explore advanced React concepts", completed: false },
-  { text: "Deploy a React app", completed: false },
-  { text: "Learn Material-UI", completed: true },
-  { text: "Optimize React performance", completed: false },
-  { text: "Master React hooks", completed: false },
-  { text: "Build a portfolio project", completed: true },
+  { title: "Learn React basicsLearn React basicsLearn React basicsLearn React basicsLearn React basics", completed: false },
+  { title: "Build a To-Do List app", completed: true },
+  { title: "Explore advanced React concepts", completed: false },
+  { title: "Deploy a React app", completed: false },
+  { title: "Learn Material-UI", completed: true },
+  { title: "Optimize React performance", completed: false },
+  { title: "Master React hooks", completed: false },
+  { title: "Build a portfolio project", completed: true },
 ];
 
 export default ListOfTasks;
