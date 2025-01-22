21a9720a3fb8a2f7d279879cc03580fdc9ef4750
jabez-abija2399 <jabeznew2399@gmail.com>
2025-01-22 21:25:54 +0300

    feat: add AddTaskForm component for creating new tasks

---
+++ src/components/AddTaskForm.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import { Box, Button, TextField, Typography, Paper } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
+const AddTaskForm = ({ tasks, setTasks }) => {
+  const [task, setTask] = useState("");
+  const navigate = useNavigate();
+
+  const handleInputChange = (event) => {
+    setTask(event.target.value); // Update task input value
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Prevent page reload
+    if (task.trim() !== "") {
+        const newTask = { title: task, completed: false };
+        setTasks([...tasks, newTask]); // Add new task to the tasks array
+        navigate("/"); // Navigate back to task list
+    }
+  };
+
+  return (
+    <Box
+      component={Paper}
+      sx={{
+        maxWidth: 600,
+        margin: "20px auto",
+        boxShadow: 3,
+        borderRadius: 10,
+        padding: 3,
+      }}
+    >
+      <Typography variant="h5" align="center" gutterBottom>
+        Add New Task
+      </Typography>
+
+      <form onSubmit={handleSubmit}>
+        <Box sx={{ display: "flex", maxWidth: 400, margin: "20px auto" }}>
+          <TextField
+            label="Task"
+            variant="outlined"
+            fullWidth
+            helperText="Enter a new task"
+            value={task}
+            onChange={handleInputChange}
+            sx={{ marginBottom: 2}}
+
+          />
+        </Box>
+
+        <Box sx={{ display: "flex", maxWidth: 200, margin: "20px auto" }}>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            fullWidth
+            sx={{ backgroundColor: "#384727dc" }}
+          >
+            Add Task
+          </Button>
+          <Button
+          variant="outlined"
+          color="black"
+          sx={{ marginLeft: 2, backgroundColor: "red" }}
+          onClick={() => navigate("/")}
+        >
+          Cancel
+        </Button>
+        </Box>
+      </form>
+    </Box>
+  );
+};
+
+export default AddTaskForm;
