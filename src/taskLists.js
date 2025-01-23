d0f794ff99f1ad74f1264b5f67479f405daa6ea4
jabez-abija2399 <jabeznew2399@gmail.com>
2025-01-23 11:28:15 +0300

    feat: add unique IDs to tasks in ListOfTasks for better identification

--- src/taskLists.js
+++ src/taskLists.js
@@ -1,12 +1,12 @@
 const ListOfTasks = [
-  { title: "Learn React basicsLearn React basicsLearn React basicsLearn React basicsLearn React basics", completed: false },
-  { title: "Build a To-Do List app", completed: true },
-  { title: "Explore advanced React concepts", completed: false },
-  { title: "Deploy a React app", completed: false },
-  { title: "Learn Material-UI", completed: true },
-  { title: "Optimize React performance", completed: false },
-  { title: "Master React hooks", completed: false },
-  { title: "Build a portfolio project", completed: true },
+  {id: 1, title: "Learn React basicsLearn React basicsLearn React basicsLearn React basicsLearn React basics", completed: false },
+  {id: 2, title: "Build a To-Do List app", completed: true },
+  {id: 3, title: "Explore advanced React concepts", completed: false },
+  {id: 4, title: "Deploy a React app", completed: false },
+  {id: 5, title: "Learn Material-UI", completed: true },
+  {id: 6, title: "Optimize React performance", completed: false },
+  {id: 7, title: "Master React hooks", completed: false },
+  {id: 8, title: "Build a portfolio project", completed: true },
 ];
 
 export default ListOfTasks;
